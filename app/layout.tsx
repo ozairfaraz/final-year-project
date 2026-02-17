ad2a07a02cfa925ae6dfa76ec76d98847383338a
Faraz Ozair <farazozair5@gmail.com>
2026-02-17 19:29:06 +0530

    feat: add Navbar and Theme components with dropdown functionality
    - Updated package.json to include new dependencies: class-variance-authority, clsx, lucide-react, next-themes, radix-ui, tailwind-merge, shadcn, and tw-animate-css.
    - Created components.json for Shadcn configuration.
    - Implemented Navbar component for site navigation.
    - Added Theme component for theme switching using next-themes.
    - Developed reusable Button and DropdownMenu components with Radix UI.
    - Introduced ThemeProvider context for managing theme state.
    - Added utility functions for class name merging.
    used Shadcn with next themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { ThemeProvider } from "../context/themeProvider";
 import "./globals.css";
+import Navbar from "@/components/navigation/navbar";
 
 const inter = localFont({
   src: "./fonts/interVF.ttf",
@@ -16,10 +18,11 @@ const spaceGrotesk = localFont({
 
 export const metadata: Metadata = {
   title: "DevFlow",
-  description: "A platform for developers for asking and answering questions,collaborating on projects, and sharing knowledge. Explore a wide range of topics, from programming languages and frameworks to best practices and industry trends.",
-  icons:{
+  description:
+    "A platform for developers for asking and answering questions,collaborating on projects, and sharing knowledge. Explore a wide range of topics, from programming languages and frameworks to best practices and industry trends.",
+  icons: {
     icon: "/app/favicon.ico",
-  }
+  },
 };
 
 export default function RootLayout({
@@ -28,12 +31,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         suppressHydrationWarning
         className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
